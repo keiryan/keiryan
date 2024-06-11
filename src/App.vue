5cfdbd2e7b1f9cbf9b69d7c647af9fdc2853ca25
keiryan wilson <keiryanwilson@gmail.com>
2024-06-11 01:05:37 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-screen h-screen flex items-center justify-center text-2xl underline font-bold">
+    Keiryan Coming Soon
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>

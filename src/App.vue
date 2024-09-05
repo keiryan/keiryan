cf18aa68017f3d2e16c3e46c1e63ba19e95a3598
keiryan wilson <keiryanwilson@gmail.com>
2024-09-05 10:48:11 -0400

    testing homepage push

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="w-screen h-screen flex items-center justify-center text-2xl underline font-bold">
-    Keiryan is Coming Soon
+  <div
+    class="w-screen h-screen flex flex-col justify-between overflow-y-auto font-['arial'] text-[#1C1C1C]"
+  >
+    <!-- Middle Div -->
+    <div class="max-w-screen-2xl w-full p-4">
+      <!-- <Nav />
+      <Home /> -->
+      This is a test again
+    </div>
+    <Footer />
   </div>
 </template>
 
-<script setup></script>
+<script>
+import Nav from "./components/Nav.vue";
+import Home from "./Pages/Home.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Nav,
+    Home,
+    Footer,
+  },
+};
+</script>
 
 <style scoped></style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <nav class="flex items-center justify-between flex-wrap py-6">
+      <h1 class="text-3xl">Keiryan Wilson</h1>
+      <ul class="flex gap-4">
+        <li>Creative</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+</script>
+
+<script></script>
+
+<style></style>
